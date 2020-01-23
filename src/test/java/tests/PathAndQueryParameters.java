a6322fdfad59c23c87e41c015476372a77be1d94
CybertekSchool <github@cybertekschool.com>
2020-01-23 14:27:04 -0500

    query param test

--- src/test/java/tests/PathAndQueryParameters.java
+++ src/test/java/tests/PathAndQueryParameters.java
@@ -36,4 +36,40 @@ public class PathAndQueryParameters {
         response.prettyPeek().then().assertThat().statusCode(400);
 
     }
+
+    /**
+     * given i create request with query parameter base=USD
+     * when i send my request to http://api.openrates.io/latest?base
+     * then the response should contain "base": "USD"
+     */
+    @Test
+    public void queryParams(){
+
+        Response response = given().queryParams("base", "USD").
+                            when().get("http://api.openrates.io/latest");
+        response.prettyPeek();
+        assertTrue(response.asString().contains("\"base\":\"USD\""));
+    }
+
+    /**
+     * given i create request with query parameter base=USD and symbol=MYR
+     * when i send my request to http://api.openrates.io/latest
+     * then the response should contain "base": "USD"
+     * and body should contain MYR
+     * but should not contain EUR
+     */
+    @Test
+    public void test2queryParams(){
+        Response response = given().queryParams("base", "USD")
+                .queryParams("symbols", "MYR")
+                .when().get("http://api.openrates.io/latest");
+        response.prettyPeek();
+        String responseStr = response.asString();
+        assertTrue(responseStr.contains("USD") && responseStr.contains("MYR"));
+        assertFalse(responseStr.contains("EUR"));
+
+    }
+
+
+
 }
