793d3fffab1611c99594829aa0ea8f249cf10aa2
CybertekSchool <github@cybertekschool.com>
2020-01-24 14:41:01 -0500

    log 2

--- src/test/java/tests/day2/LogExamples.java
+++ src/test/java/tests/day2/LogExamples.java
@@ -8,7 +8,7 @@ import static io.restassured.RestAssured.when;
 public class LogExamples {
 
     @Test
-    public void test1(){
+    public void test1() {
         // request logging
         // log().all(). --> prints everything in the request
         given().log().everything().
@@ -18,4 +18,19 @@ public class LogExamples {
                 statusCode(200);
 
     }
+
+    @Test
+    public void test2() {
+        // response logging
+        // log().all(). --> prints everything in the response
+        // log().ifError(). --> prints if we get a error status code like 4xx or 5xx
+        // ifStatusCodeIsEqualTo(401). --> prints if the status code matches the provided one
+        // log().ifValidationFails --> prints if any assertion fails
+
+        given().pathParam("id", "234237").
+                when().get("http://ec2-34-201-69-55.compute-1.amazonaws.com:1000/ords/hr/regions/{id}").
+                then().log().ifValidationFails().
+                statusCode(404);
+    }
+
 }
