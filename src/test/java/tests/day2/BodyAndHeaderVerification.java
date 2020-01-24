bc420b675fe20a27d6251b5e460153a959c6815d
CybertekSchool <github@cybertekschool.com>
2020-01-24 16:44:30 -0500

    jsonpath 2

--- src/test/java/tests/day2/BodyAndHeaderVerification.java
+++ src/test/java/tests/day2/BodyAndHeaderVerification.java
@@ -5,12 +5,17 @@ import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class BodyAndHeaderVerification {
     @BeforeAll
-    public static void setUp(){
+    public static void setUp() {
         RestAssured.baseURI = "http://ec2-34-201-69-55.compute-1.amazonaws.com:1000/ords/hr";
     }
 
@@ -24,14 +29,14 @@ public class BodyAndHeaderVerification {
                 then().
                 assertThat().statusCode(200).
                 // extract the header value content type and verify
-                and().header("Content-Type", equalTo("application/json")).
+                        and().header("Content-Type", equalTo("application/json")).
                 // extract the value of the key first_name and verify
-                and().body("first_name", equalTo("Neena"));
+                        and().body("first_name", equalTo("Neena"));
 
     }
 
     @Test
-    public void test2(){
+    public void test2() {
         JsonPath jsonPath = given().pathParam("id", "101").
                 when().get("/employees/{id}").jsonPath();
         // Jsonpath --> class used to navigate through json body and extract values
@@ -43,5 +48,44 @@ public class BodyAndHeaderVerification {
 
         String salary = jsonPath.getString("salary");
         System.out.println(salary);
+
+        System.out.println(jsonPath.getString("links.href"));
+    }
+
+    //  Using the ORDS api verify that Nancy  is the manager of the finance department.
+    // get info of the finance dep
+    // get the manager_id
+    // get the person with this id
+
+    @Test
+    public void managerTest() {
+        // get info of the finance dep
+        JsonPath jsonPath = given().pathParam("id", "100").
+                when().get("/departments/{id}").jsonPath();
+
+        String manager_id = jsonPath.getString("manager_id");
+        System.out.println("manager_id = " + manager_id);
+
+        jsonPath = given().pathParam("id", manager_id).
+                when().get("/employees/{id}").jsonPath();
+
+        assertThat(jsonPath.getString("first_name"), is("Nancy"));
+
+    }
+
+
+    @Test
+    public void moreJsonPath() {
+        JsonPath jsonPath = when().get("/countries/").jsonPath();
+        // get all countries in single string
+        String allCountries = jsonPath.getString("items.country_name");
+        System.out.println(allCountries);
+
+
+        // get all countries into list of strings
+        List<String> countriesList = jsonPath.getList("items.country_name");
+        System.out.println(countriesList.size());
+
+
     }
 }
