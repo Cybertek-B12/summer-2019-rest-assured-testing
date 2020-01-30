b7b385c952211152d72054d8504144601e9d4b18
CybertekSchool <github@cybertekschool.com>
2020-01-30 11:56:30 -0500

    hello from sparta testtt list

--- src/test/java/tests/day5/ThisIsTestTTT.java
+++ src/test/java/tests/day5/ThisIsTestTTT.java
@@ -11,7 +11,7 @@ import static org.hamcrest.Matchers.*;
 public class ThisIsTestTTT {
 
     @BeforeAll
-    public static void setUp(){
+    public static void setUp() {
         baseURI = "http://ec2-34-201-69-55.compute-1.amazonaws.com:8000/";
     }
 
@@ -21,37 +21,53 @@ public class ThisIsTestTTT {
      * verify status code 200
      */
     @Test
-    public void getAllSpartanssss(){
+    public void getAllSpartanssss() {
         get("/api/spartans").prettyPeek().then().statusCode(200);
     }
 
     /**
      * get spartan with id 132
      * verify that response header
-     *      content type - application.json
-     *
-    *      verify that response body
-     *      "id": 16,
-     *         "name": "Sinclair",
-     *         "gender": "Male",
-     *         "phone": 9714460354
-     *
-     *
+     * content type - application.json
+     * <p>
+     * verify that response body
+     * "id": 16,
+     * "name": "Sinclair",
+     * "gender": "Male",
+     * "phone": 9714460354
      */
 
     @Test
-    public void getSingleSpartannn(){
+    public void getSingleSpartannn() {
         given().pathParam("id", 132).
-            when().get("/api/spartans/{id}").
+                when().get("/api/spartans/{id}").
                 prettyPeek().
-            then().statusCode(200).
-            contentType(ContentType.JSON).
-            body("id", equalTo(132)).
-            body("name", equalTo("Sinclair")).
-            body("gender", equalTo("Male")).
-            body("phone", equalTo(9714460354L));
+                then().statusCode(200).
+                contentType(ContentType.JSON).
+                body("id", equalTo(132)).
+                body("name", equalTo("Sinclair")).
+                body("gender", equalTo("Male")).
+                body("phone", equalTo(9714460354L));
     }
 
+    /**
+     * make a get request to search api
+     * query param nameContains : ha
+     * verify
+     * response status code 200
+     * header application/json
+     * body:   names of all object returns contain the string
+     */
 
+    @Test
+    public void searchForHaaa() {
+        given().log().uri().
+                queryParam("nameContains", "ha").
+            when().get("/api/spartans/search").
+                prettyPeek().
+            then().statusCode(200).
+                contentType(ContentType.JSON).
+                body("content.name", everyItem(containsStringIgnoringCase("ha")));
+    }
 
 }
