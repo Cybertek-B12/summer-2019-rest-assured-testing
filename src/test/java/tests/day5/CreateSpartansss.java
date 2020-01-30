3c6886257d17a6d0c3996b30239b2afc9e5e2376
CybertekSchool <github@cybertekschool.com>
2020-01-30 15:03:06 -0500

    hello from sparta testtt post using map

--- src/test/java/tests/day5/CreateSpartansss.java
+++ src/test/java/tests/day5/CreateSpartansss.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.*;
@@ -19,7 +21,7 @@ public class CreateSpartansss {
 
     /**
      * create a new spartan  object
-     * by passing valid gender, name, phone
+     * by passing valid gender, name, phone from file
      * verify response body
      *  success = A Spartan is Born!
      */
@@ -28,13 +30,36 @@ public class CreateSpartansss {
         /// create the File that we want to send
         File jsonFile = new File("src/test/resources/spartan.json");
 
-        given().contentType(ContentType.JSON).
+        given().log().all()
+                .contentType(ContentType.JSON).
                 // include the file in the response
                 body(jsonFile).
             when().post("/api/spartans").
                 prettyPeek().
             then().statusCode(201).
                     body("success", equalTo("A Spartan is Born!"));
+    }
+
+    /**
+     * create a new spartan  object
+     * by passing valid gender, name, phone from file
+     * verify response body
+     *  success = A Spartan is Born!
+     */
+    @Test
+    public void postANewSpartannnn(){
+        Map<String, String> spartan = new HashMap<>();
+        spartan.put("gender", "Female");
+        spartan.put("name", "Rosa");
+        spartan.put("phone", "202323232232");
+
+        given().log().all().
+                contentType(ContentType.JSON).
+                body(spartan).
+            when().post("/api/spartans").
+                    prettyPeek().
+            then().statusCode(201);
+
     }
 
 }
