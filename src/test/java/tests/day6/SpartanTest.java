2a6ec7372f4114a8c1bb48c1d36339b49b54d1b2
CybertekSchool <github@cybertekschool.com>
2020-01-31 14:29:18 -0500

    put example

--- src/test/java/tests/day6/SpartanTest.java
+++ src/test/java/tests/day6/SpartanTest.java
@@ -2,6 +2,7 @@ package tests.day6;
 
 import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -11,16 +12,17 @@ import utilities.SpartanApiUtils;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class SpartanTest {
 
     @BeforeAll
-    public static void setup(){
+    public static void setup() {
         RestAssured.baseURI = ConfigurationReader.get("spartanApiBaseURL");
     }
 
     @Test
-    public void createSpartanTest(){
+    public void createSpartanTest() {
         Faker faker = new Faker();
         String name = faker.name().firstName();
         String gender = "Female";
@@ -42,7 +44,7 @@ public class SpartanTest {
      * call the get spartan api and verify code 404
      */
     @Test
-    public void deleteTest(){
+    public void deleteTest() {
         // creating the pojo
         Spartan spartan = SpartanApiUtils.createSpartanObject();
         // send request to api tp create a spartan
@@ -52,14 +54,60 @@ public class SpartanTest {
         int id = postResponse.path("data.id");
 
         given().pathParam("id", id).
-            when().delete("/api/spartans/{id}").
-            then().statusCode(204);
+                when().delete("/api/spartans/{id}").
+                then().statusCode(204);
 
         // call the get spartan api and verify code 404
 
         given().pathParam("id", id).
+                when().get("/api/spartans/{id}").
+                then().statusCode(404);
+    }
+
+    /**
+     * UPDATE USING PUT
+     * create new spartan
+     * <p>
+     * update the name
+     * <p>
+     * verify name is updated
+     * verify other information is not update
+     */
+
+    @Test
+    public void putTest() {
+        // creating the pojo
+        Spartan spartan = SpartanApiUtils.createSpartanObject();
+        // send request to api tp create a spartan
+        Response postResponse = SpartanApiUtils.createSpartan(spartan);
+        postResponse.then().statusCode(201);
+
+        int id = postResponse.path("data.id");
+
+
+        spartan.setName("Zoom");
+        // send the new information using put
+
+        given().contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                pathParam("id", id).
+                body(spartan).
+        when().put("/api/spartans/{id}").
+            prettyPeek().
+        then().statusCode(204);
+
+        // verify the updated by getting the spartan by id
+//             * verify name is updated
+//     * verify other information is not update
+        given().pathParam("id", id).
+                accept(ContentType.JSON).
             when().get("/api/spartans/{id}").
-            then().statusCode(404);
+                prettyPeek().
+            then().statusCode(200).
+                body("name", equalTo(spartan.getName())).
+                body("gender", equalTo(spartan.getGender())).
+                body("phone", is(spartan.getPhone()));
+
     }
 
 
