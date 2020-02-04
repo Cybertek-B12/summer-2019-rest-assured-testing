9950bff8f7a83d3d83d4ec2475413c45131f2d74
CybertekSchool <github@cybertekschool.com>
2020-02-04 10:40:12 -0500

    testing get student endpoint

--- src/test/java/tests/day8/BookitStudentTests.java
+++ src/test/java/tests/day8/BookitStudentTests.java
@@ -2,11 +2,14 @@ package tests.day8;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import utilities.ConfigurationReader;
 import utilities.TokenUtility;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
@@ -38,14 +41,16 @@ public class BookitStudentTests {
         assertThat(token, not(emptyOrNullString()));
         // get all students
 
-        given().
+       Response response = given().
                 header("Authorization", token).
         when().
-                get("/api/students").
-                prettyPeek().
-        then().
-                statusCode(200).
-                contentType(ContentType.JSON);
+                get("/api/students");
+        response.prettyPeek();
+        response.then().
+                    statusCode(200).
+                    contentType(ContentType.JSON);
+        List<String> firstNames = response.jsonPath().getList("firstName");
+        System.out.println("firstNames.size() = " + firstNames.size());
 
     }
 }
