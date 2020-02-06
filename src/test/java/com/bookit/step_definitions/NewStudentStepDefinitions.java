93d5fd1bf58a5e293cd5353ba415667ea19974b9
CybertekSchool <github@cybertekschool.com>
2020-02-06 14:36:28 -0500

    create new student utility

---
+++ src/test/java/com/bookit/step_definitions/NewStudentStepDefinitions.java
@@ -0,0 +1,20 @@
+package com.bookit.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class NewStudentStepDefinitions {
+
+    @Given("I have a token as a teacher")
+    public void i_have_a_token_as_a_teacher() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the response should contain student name")
+    public void the_response_should_contain_student_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+}
--- src/test/resources/features/NewStudent.feature
+++ src/test/resources/features/NewStudent.feature
@@ -1,9 +1,14 @@
 @ws
 Feature: Create a new student
 
-  @post_student
   Scenario: student permission
     Given I have a token as a student team member
-    And I post a new student using "/api/students/student"
+    When I post a new student using "/api/students/student"
     Then the response code should be 403
 
+  @post_student
+    Scenario: Create and verify new student info
+    Given I have a token as a teacher
+    When I post a new student using "/api/students/student"
+    Then the response code should be 201
+    And the response should contain student name
